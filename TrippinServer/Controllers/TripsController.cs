e4ead51fefdc3f30290c242828843976a04bcdc0
omerhaimovich <Omer.haimovich@gmail.com>
2017-05-02 21:54:44 +0300

    b

--- TrippinServer/Controllers/TripsController.cs
+++ TrippinServer/Controllers/TripsController.cs
@@ -8,21 +8,29 @@ using System.Net.Http;
 using System.Web.Http;
 using MongoDB.Driver;
 using TrippinServer.Models;
+using Algorithm.Trips;
 
 namespace TrippinServer.Controllers
 {
     public class TripsController : ApiController
     {
         [HttpPost]
-        [Route("GetTrip")]
+        [Route("CreateTrip")]
         // http://host:port/Users/GetTrip
-        public IHttpActionResult GetTrip([FromBody] CreateTripRequest p_objTripCreationRequest)
+        public IHttpActionResult CreateTrip([FromBody] CreateTripRequest p_objTripCreationRequest)
         {
-            
-
             // Returns Trip
-            return Ok();
+            return Ok(TripsBL.CreateTrip(p_objTripCreationRequest.UserEmail, p_objTripCreationRequest.Lat, p_objTripCreationRequest.Lng));
         }
+
+        //[HttpGet]
+        //[Route("GetTrip")]
+        // http://host:port/Users/GetTrip
+        //public IHttpActionResult GetTrip([FromBody] CreateTripRequest p_objTripCreationRequest)
+        //{
+            // Returns Trip
+            //return Ok(TripsBL.GetTrip(p_objTripCreationRequest.UserEmail, p_objTripCreationRequest.Lat, p_objTripCreationRequest.Lng));
+        //}
 
 
 
